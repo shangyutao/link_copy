36adc642875922fe3bf0e9bc487f5e3c6edfa90e
shangyutao <1218622245@qq.com>
2025-06-29 01:42:46 +0800

    更新API配置：HTTP改为HTTPS

--- dist/assets/web-91c15f9e.js
+++
@@ -1,2 +0,0 @@
-import{W as n}from"./index-29d93a64.js";import"./vendor-194b799d.js";import"./vant-83faff4e.js";class t extends n{async show(e){}async hide(e){}}export{t as SplashScreenWeb};
-//# sourceMappingURL=web-91c15f9e.js.map
